946e5dcbfb4c9b1c3884664b3d30f9128e63a501
Decodon <20095352@mail.wit.ie>
2022-08-03 21:25:33 +0100

    Enable donation-svelte to authenticate to donation-hapi

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import {setContext} from "svelte";
+  import {DonationService} from "./services/donation-service";
+  import Main from "./pages/Main.svelte"
+  import Login from "./pages/Login.svelte"
+  import Signup from "./pages/Signup.svelte"
+  import Donate from "./pages/Donate.svelte";
+  import Report from "./pages/Report.svelte";
+  import Router from "svelte-spa-router";
+
+  setContext("DonationService", new DonationService("http://localhost:4000"));
+
+  let routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/main": Main,
+    "/donate": Donate,
+    "/report": Report,
+    "/logout": Main
+  }
+</script>
+
+<div class="container">
+  <Router {routes}/>
+</div>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,28 @@
+<script>
+    import homer from "/src/assets/homer.png";
+    import WelcomeNavigator from "../components/WelcomeNavigator.svelte";
+    import TitleBar from "../components/TitleBar.svelte";
+    import {getContext} from "svelte";
+
+    const donationService = getContext("DonationService");
+    donationService.logout();
+</script>
+
+<div class="columns is-vcentered">
+    <div class="column is-two-thirds">
+        <TitleBar title={"Donation Services Inc."} subTitle={"Sign up or Log in"}/>
+    </div>
+    <div class="column">
+        <WelcomeNavigator/>
+    </div>
+</div>
+
+<div class="columns is-vcentered content">
+    <div class="column has-text-centered">
+        <img width="300" src="{homer}" alt="homer">
+    </div>
+    <div class="column">
+        <h1 class="title">Help Me Run Springfield</h1>
+        <p>Donate what you can now - No Bitcoins accepted!</p>
+    </div>
+</div>
